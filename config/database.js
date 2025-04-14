e84cabb7432fb99ac90e324ac88d8d53b649af97
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-15 00:44:19 +0300

    nothing important

--- config/database.js
+++ config/database.js
@@ -1,21 +1,25 @@
-import { Sequelize }  from "sequelize";
+import { Sequelize } from "sequelize";
 
-
-// const { Sequelize } = require("sequelize");
-
-
-const sequelize = new Sequelize("postgres", "postgres", "mohamed", {
-  host: "localhost",
-  dialect: "postgres", // Tells Sequelize we're using PostgreSQL
-  logging: false, // Disable SQL query logs (optional)
-});
-
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // needed for Render
+      },
+    },
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log("✅ Connected to PostgreSQL successfully!"))
   .catch(err => console.error("❌ Unable to connect:", err));
 
-
-export default sequelize; 
-//module.exports = sequelize;
- 
+export default sequelize;
