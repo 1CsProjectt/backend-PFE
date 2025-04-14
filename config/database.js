a4ed74408fd1425d97e82b9f293e9bbfa5954e77
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-15 01:24:15 +0300

    nothing important

--- config/database.js
+++ config/database.js
@@ -1,4 +1,22 @@
 import { Sequelize } from "sequelize";
+import sequelize from "./database.js";
+import User from "../models/UserModel.js"; // Import the model
+import PFE from "../models/PFEmodel.js";
+import Event from "../models/eventModel.js";
+import Student from "../models/studenModel.js";
+import teacher from "../models/teacherModel.js";
+import Company from "../models/companyModel.js";
+import Admin from "../models/adminModel.js";
+import JoinRequest from "../models/jointeamModel.js";
+import invitation from "../models/invitationModel.js";
+
+
+
+
+
+
+
+
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -21,5 +39,17 @@ const sequelize = new Sequelize(
 sequelize.authenticate()
   .then(() => console.log("✅ Connected to PostgreSQL successfully!"))
   .catch(err => console.error("❌ Unable to connect:", err));
+
+  (async () => {
+    try {
+      await sequelize.sync({  alter: true }); // Creates or updates tables 
+      console.log("✅ Database synced!");
+      
+    } catch (error) {
+      console.error("❌ Sync error:", error); 
+    } finally {
+      await sequelize.close(); // Close the database connection
+    }
+  })();
 
 export default sequelize;
--- config/sync.js
+++ config/sync.js
@@ -10,14 +10,7 @@ import JoinRequest from "../models/jointeamModel.js";
 import invitation from "../models/invitationModel.js";
 
 
-// const sequelize = require("../database.js");
-// const User = require("../models/UserModel.js"); 
-// const PFE = require("../models/PFEmodel.js");
-// const Event = require("../models/eventModel.js");
-// const Student = require("../models/studenModel.js");
-// const Teacher = require("../models/teacherModel.js");
-// const Company = require("../models/companyModel.js");
-// const Admin = require("../models/adminModel.js");
+
 
 
 
