1b3f3d986c79df75516b0ba430ab137751f47e5a
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-15 01:02:20 +0300

    nothing important

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const allowedOrigins = [
   "http://localhost:5000",
   "http://192.168.206.209:3000", 
   "http://192.168.170.167:3000",
-  "https://8cb9-154-247-119-87.ngrok-free.app",
+  "https://backend-pfe-1.onrender.com",
   "https://180b-154-247-119-87.ngrok-free.app",
   "https://6072-105-235-138-57.ngrok-free.app/api/v1" ,
   "https://9625-105-235-138-133.ngrok-free.app/api/v1"
