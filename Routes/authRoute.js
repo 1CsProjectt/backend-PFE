30de68dc545ffea14746513731fa79f786dfb860
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-15 22:44:14 +0300

    nothing important

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { login } from '../controllers/authentification.js';
-import { forgotPassword,validateResetToken,resetPassword } from '../controllers/authentification.js';
+import { forgotPassword,validateResetToken,resetPassword,getMe } from '../controllers/authentification.js';
 // const express = require('express');
 // const { login } = require('../controllers/authentification.js');
-
+import { protect } from '../middlewares/authmiddleware.js';
 
 const router = express.Router();
 
@@ -12,7 +12,7 @@ router.post("/forgot-password", forgotPassword);
 router.get("/reset-password/:token",validateResetToken);
 router.post("/reset-password/:token",resetPassword);
 
-
+router.get("/me", protect ,getMe);
 
 export default router;
 
