add141433600267b8a8b103b1d715618b6602f85
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-19 10:59:32 +0300

    updating pfe status

---
+++ utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: 'pfe-uploads',
+      resource_type: file.mimetype === 'application/pdf' ? 'raw' : 'image',
+      public_id: `${Date.now()}-${file.originalname.split('.')[0]}`,
+    };
+  },
+});
+
+export const upload = multer({ storage });
