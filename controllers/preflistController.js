fe3ee3cb3a7b149ccdcd5a39723afca41fad35e3
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-27 22:06:46 +0100

    abdouuu tiriri

--- controllers/preflistController.js
+++ controllers/preflistController.js
@@ -720,6 +720,7 @@ export const getMyPreflist = catchAsync(async (req, res, next) => {
           {
             model: SupervisionRequest,
             as: 'supervisionRequests', 
+            where:{teamId, pfeId: Sequelize.col('PFE.id')},
             attributes: ['status'], 
           }
         ]
