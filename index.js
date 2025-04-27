1c398b8453fd351b620030e379e4814a126a4d80
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-27 21:08:51 +0100

    abdouuu tiriri

--- index.js
+++ index.js
@@ -159,10 +159,10 @@ app.use(session({
 
 // Routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/v1/auth', authRoutes);
 app.use(protect,getCurrentSession);
 app.use(injectCurrentSession);
 app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/pfe', pfeRoutes);
 app.use("/api/v1/session", eventRoutes);
 app.use("/api/v1/student", studentroute);
