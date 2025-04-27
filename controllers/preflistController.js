7f4e42b328a33e81ac46558ab2397561833b3f02
boumohamed19 <md.bouaouadja@esi-sba.dz>
2025-04-27 20:13:58 +0100

    abdouuu tiriri

--- controllers/preflistController.js
+++ controllers/preflistController.js
@@ -709,6 +709,7 @@ export const getMyPreflist = catchAsync(async (req, res, next) => {
     include: [
       {
         model: PFE, 
+        as: 'assignedPFE',
         attributes: ['id', 'title', 'description', 'year', 'specialization',"photo","pdfFile"],
         include: [
           {
